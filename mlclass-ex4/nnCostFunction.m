efc389eb0d9a5a3ae0c85d8abb330225978a217f
David JY Lu <davidlue7@gmail.com>
2014-04-15 13:22:56 +0800

    add binary matrix by eye()
    Signed-off-by: David JY Lu <davidlue7@gmail.com>

--- mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4/nnCostFunction.m
@@ -68,11 +68,10 @@ a2 = sigmoid(z2);
 a2 = [ones(m, 1) a2];
 z3 = a2*Theta2';
 a3 = sigmoid(z3);
-y_k = zeros(m,num_labels);
+y_k = eye(num_labels)(y,:);
 
 for c=1:num_labels,
-	y_c = (y==c);
-	y_k(:,c) = y_c;
+	y_c = y_k(:,c);
         output = a3(:,c);
 	J = J + sum(-y_c.*log(output)-(1-y_c).*log(1-output))/m;
 end
