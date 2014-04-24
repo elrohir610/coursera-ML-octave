ef956b7f17aa0cfe30ef46855df35265b6816f26
David JY Lu <davidlue7@gmail.com>
2014-04-24 14:07:06 +0800

    ex7 done
    Signed-off-by: David JY Lu <davidlue7@gmail.com>

--- mlclass-ex6/dataset3Params.m
+++ mlclass-ex6/dataset3Params.m
@@ -23,9 +23,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+min_err = length(y);
 
+for i = 1:length(C_vec),
+	for j = 1:length(sigma_vec),
+		model= svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j)));
+		predictions = svmPredict(model, Xval);
+		curr_err = mean(double(predictions ~= yval));
 
-
+		if curr_err < min_err,
+			C = C_vec(i);
+			sigma = sigma_vec(j);
+			min_err = curr_err;
+		end
+	end
+end
 
 
 
